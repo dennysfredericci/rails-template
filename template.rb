3be85d018acc1684577996f96b0ab6099ad40892
Dennys <dennys@fredericci.com.br>
2012-09-02 23:33:02 -0300

    add web server and fix heroku config

--- template.rb
+++ template.rb
@@ -12,40 +12,17 @@ def add_twitter_bootstrap
   generate "bootstrap:layout application --force"
 end
 
+def add_web_server
+  gem "thin"
+end
+
 def add_devise
   gem "devise"
   generate "devise:install"
   #model_name = ask("Qual o nome do model você quer usar? [usuario]")
   model_name = "usuario" if model_name.blank?
   generate("devise", model_name)
-end
-
-def add_web_server
-  gem "thin"
-end
-
-def add_heroku_config
-  gem "mysql2"
-  replace_content_from_file "Gemfile", /gem 'sqlite3'/, "gem 'pg'"
-  database = YAML.load_file('config/database.yml')
-  database["production"].clear
-  database["production"] = {  "adapter" => "pq", "encoding" => "utf8", "database" => "ENV['DATABASE_URL']", "pool" => 5, "timeout" => 5000 }
-
-  File.open('config/database.yml', 'w') do |out|
-    YAML.dump(database, out)
-  end
-
-  run "bundle install"
-
-end
-
-def heroku_deploy
-  git :add => "."
-  git :commit => "-a -m 'Adding heroku config'"
-  run "heroku create #{@app_name}"
-  git :push => 'heroku master'
-  run 'heroku run rake db:migrate'
-  run 'heroku open'
+  generate("devise:views", model_name)
 end
 
 def custom_config
@@ -59,12 +36,51 @@ def custom_config
   git :commit => "-a -m 'Initial commit'"
 end
 
+def add_heroku_config
+
+  gem "mysql2"
+  replace_content_from_file "Gemfile", /gem 'sqlite3'/, "gem 'pg'"
+
+  database = YAML.load_file('config/database.yml')
+
+  database["development"].clear
+  database["development"] = { "adapter" => "mysql2", "encoding"  => "utf8", "database" => "#{@app_name}_dev", "username"  => "root", "password"  => "root", "host"  => "127.0.0.1", "port" => 3306, "pool" => 5, "timeout" => 5000 }
+
+  database["test"].clear
+  database["test"] = { "adapter" => "mysql2", "encoding"  => "utf8", "database" => "#{@app_name}_test", "username"  => "root", "password"  => "root", "host"  => "127.0.0.1", "port" => 3306, "pool" => 5, "timeout" => 5000 }
+
+  database["production"].clear
+  database["production"] = {  "adapter" => "postgresql", "encoding" => "utf8", "database" => "ENV['DATABASE_URL']", "pool" => 5, "timeout" => 5000 }
+
+  ## change to mysql2 to others environment
+
+  File.open('config/database.yml', 'w') do |out|
+    YAML.dump(database, out)
+  end
+
+  run "bundle install"
+  run 'bundle exec rake db:drop'
+  run 'bundle exec rake db:create'
+
+end
+
+def heroku_deploy
+
+  add_heroku_config
+
+  git :add => "."
+  git :commit => "-a -m 'Adding heroku config'"
+  run "heroku create #{@app_name}"
+  git :push => 'heroku master'
+  run 'heroku run rake db:migrate'
+  run 'heroku open'
+end
+
 def main
+  add_web_server
   add_twitter_bootstrap
   add_devise
-  add_web_server
   custom_config
-  add_heroku_config
   heroku_deploy
 end
 
